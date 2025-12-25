2e54910cd6dd48c0d2e7e9d5eb3092ef000bb143
hassaanshaikh <hassaanh890@gmail.com>
2025-12-25 07:32:52 -0800

    new

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -33,7 +33,8 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { Mail, Lock } from "lucide-react";
 
 export default function Register() {
   const [email, setEmail] = useState('');
@@ -57,18 +58,48 @@ export default function Register() {
 
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Register</h2>
+    <div className="min-h-screen flex items-center justify-center bg-white via-gray-900 to-black text-white">
+      <div className="w-full max-w-md bg-gray-900/80 backdrop-blur border border-gray-800 rounded-2xl shadow-2xl p-8">
+      <h1 className="text-3xl font-bold text-center mb-2">
+        <span className="text-yellow-500">Smart</span>Notes Register
+      </h1>
+      <p className="text-gray-400 text-center mb-8">
+         Welcome back! Please login to your account
+      </p>
       <form onSubmit={handleRegister}>
-       
-        <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} required /><br /><br />
 
-        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required /><br /><br />
-        
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required /><br /><br />
-        
-        <button type="submit">Register</button>
+                <div className="mb-5">
+          <label className="text-sm text-gray-300">Name</label>
+            <div className="flex items-center mt-2 bg-black/40 border border-gray-700 rounded-xl px-4">
+              {/* <Mail className="text-gray-400" size={18} /> */}
+                <input type="text" placeholder="Name" className="w-full bg-transparent outline-none px-3 py-3 text-sm" value={name} onChange={e => setName(e.target.value)} required />
+            </div>
+        </div>
+        <div className="mb-5">
+          <label className="text-sm text-gray-300">Email</label>
+            <div className="flex items-center mt-2 bg-black/40 border border-gray-700 rounded-xl px-4">
+              <Mail className="text-gray-400" size={18} />
+        <input type="email" placeholder="Email"  className="w-full bg-transparent outline-none px-3 py-3 text-sm" value={email} onChange={e => setEmail(e.target.value)} required />
+            </div>
+        </div>
+
+        <div className="mb-5">
+          <label className="text-sm text-gray-300">Password</label>
+            <div className="flex items-center mt-2 bg-black/40 border border-gray-700 rounded-xl px-4">
+              <Lock className="text-gray-400" size={18} />
+                      <input type="password" placeholder="Password" className="w-full bg-transparent outline-none px-3 py-3 text-sm" value={password} onChange={e => setPassword(e.target.value)} required />
+            </div>
+        </div>
+
+        <button type="submit" className="w-full bg-yellow-500 text-black py-3 rounded-xl font-semibold hover:bg-yellow-400 transition">Register</button>
+        <p className="text-center text-gray-400 text-sm mt-6">
+         Already create an account please {" "}
+         <Link to='/Login'><span className="text-yellow-500 cursor-pointer hover:underline">
+            Sign in
+           </span></Link>
+         </p>
       </form>
+      </div>
     </div>
   );
 }
